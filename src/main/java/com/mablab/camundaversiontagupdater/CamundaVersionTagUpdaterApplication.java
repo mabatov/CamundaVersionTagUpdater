070f9ed929a79d4aea56bc5bafc9005cd708d9e6
nmabatov <nikita-mabatov@mail.ru>
2023-10-16 15:24:21 +0300

    Update all by hardcoded project.version in any pom

---
+++ src/main/java/com/mablab/camundaversiontagupdater/CamundaVersionTagUpdaterApplication.java
@@ -0,0 +1,13 @@
+package com.mablab.camundaversiontagupdater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamundaVersionTagUpdaterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CamundaVersionTagUpdaterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mablab/camundaversiontagupdater/CamundaVersionTagUpdaterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mablab.camundaversiontagupdater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamundaVersionTagUpdaterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
